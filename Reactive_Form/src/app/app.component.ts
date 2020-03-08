add29558bba84626469ebf784f7e9b746a312b05
j_sandeera <j.sandeera@theglobalmarketaccess.com>
2020-03-08 17:15:27 +0530

    reactive form example

---
+++ Reactive_Form/src/app/app.component.ts
@@ -0,0 +1,66 @@
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  forbiddenUserNames = ['Chris', 'Anna'];
+  signUpForm: FormGroup;
+
+  // ngOnInit() {
+  //   this.signUpForm = new FormGroup({
+  //     username: new FormControl(null, Validators.required),
+  //     email: new FormControl(null,[Validators.required, Validators.email]),
+  //     gender: new FormControl('male')
+  //   });
+  //
+  // }
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+
+      userData: new FormGroup({
+        // here we dont call the forbiddennames function, angular will call, so it is need to bind
+        username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
+      }),
+
+      gender: new FormControl('male'),
+      hobbies: new FormArray([])
+    });
+
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+
+  // onAddHobby() {
+  //   const control = new FormControl(null, Validators.required);
+  //   (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  // }
+  // this return a key value pair
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return {nameIsForbidden: true};
+    }
+    return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'sandeerads@gmail.com') {
+          resolve({emailIsForbidden: true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+}
